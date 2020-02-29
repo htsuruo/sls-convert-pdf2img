b4fbda8a2ea34d202d101f37b8455fc5df99273c
HTsuruo <tsuru.dev@gmail.com>
2020-03-01 02:15:52 +0900

    feat: s3ファイル操作ロジックを追加.

--- handler.ts
+++ handler.ts
@@ -2,11 +2,32 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
 export const s3hook = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+  new FileManager(event)
+}
+
+
+class FileManager {
+
+  HOST = '/Users/tsuruoka/github/sls-convert-pdf2img/s3-local'
+  bucketname: string
+  key: string
+  url: string
+  filename: string
+  key_without_filename: string
+
+  constructor(event) {
+    this.bucketname = event.Records[0].s3.bucket.name
+    this.key = event.Records[0].s3.object.key
+    console.log(`bucketname: ${this.bucketname}, key: ${this.key}`)
+    this.url = `${this.HOST}/${this.bucketname}/${this.key}`
+    console.log(this.url)
+    this._split_filename()
+  }
+
+  _split_filename() {
+    const keyarr = this.key.split('/')
+    this.filename = keyarr.pop()
+    this.key_without_filename = keyarr.join('/') + '/'
+    console.log(`filename: ${this.filename}, target_dir: ${this.key_without_filename}`)
+  }
 }
