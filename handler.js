169ebe2dafb1712e5af67389496d37a5abc4093a
HTsuruo <tsuru.dev@gmail.com>
2020-03-01 00:24:39 +0900

    feat: add pdf to image logic.

--- handler.js
+++ handler.js
@@ -1,18 +1,19 @@
-const AWS = require('aws-sdk');
+const aws = require('aws-sdk');
+const pdf2image = require('./pdf2image')
 
-module.exports.webhook = (event, context, callback) => {
-  const S3 = new AWS.S3({
-    s3ForcePathStyle: true,
-    endpoint: new AWS.Endpoint('http://localhost:8000'),
-    accessKeyId: 'S3RVER',
-    secretAccessKey: 'S3RVER',
-  });
-  S3.putObject({
-    Bucket: 'local-bucket',
-    Key: 'docs/12345678',
-    Body: new Buffer('abcd'),
-  }, () => { callback(null, 'ok'); });
-};
+// module.exports.webhook = (event, context, callback) => {
+//   const S3 = new AWS.S3({
+//     s3ForcePathStyle: true,
+//     endpoint: new AWS.Endpoint('http://localhost:8000'),
+//     accessKeyId: 'S3RVER',
+//     secretAccessKey: 'S3RVER',
+//   });
+//   S3.putObject({
+//     Bucket: 'local-bucket',
+//     Key: 'docs/12345678',
+//     Body: new Buffer('abcd'),
+//   }, () => { callback(null, 'ok'); });
+// };
 
 module.exports.s3hook = (event, context) => {
   console.log(JSON.stringify(event));
